25efedd8bf5a4976963bf31fe5bb9780a32552ac
Popescu Daniel <same.dan@gmail.com>
2024-12-29 20:12:42 +0100

    Added user auth routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,44 @@ import asyncHandler from "express-async-handler";
 // @route  POST /api/users/auth
 //@ access Public
 const authUser = asyncHandler(async (req, res) => {
-  // res.status(200).json({ message: "Auth user" });
-  res.status(401);
-  throw new Error("Smth went wrong");
+  res.status(200).json({ message: "Auth user" });
+  // error example
+  // res.status(401);
+  // throw new Error("Smth went wrong");
 });
 
-export { authUser };
+// @desc   Register new user
+// @route  POST /api/users
+//@ access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register user" });
+});
+
+// @desc   Logout user
+// @route  POST /api/users/logout
+//@ access Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout user" });
+});
+
+// @desc   Get user Profile
+// @route  GET /api/users/profile
+//@ access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// @desc   UPDATE user Profile
+// @route  PUT /api/users/profile
+//@ access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 // /api/users/auth
 router.post("/auth", authUser);
+// REGISTER /api/users
+router.post("/", registerUser);
+// LOGOUT /api/users/logout
+router.post("/logout", logoutUser);
+// GET & PUT /api/users/profile
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
